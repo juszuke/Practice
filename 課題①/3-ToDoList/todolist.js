1252cafd53c562ff8d1be1cde8cfcf7242f275f0
juszuke <yusuke.ino.yusuke@gmail.com>
2020-09-13 21:39:06 +0900

    start practice 1-3

--- 課題①/3-ToDoList/todolist.js
+++ 課題①/3-ToDoList/todolist.js
@@ -1,27 +1,2 @@
 'use strict'
 
-const tasks = ['掃除', '買い物', '散歩'];
-
-function showAllTasks() {
-  console.log('=========================');
-  console.log('現在持っているのタスク一覧');
-  console.log('=========================');
-  for (let i = 0; i < tasks.length; i++) {
-    console.log(`${i} : ${tasks[i]}`);
-  }
-}
-
-function addTask() {
-  let newTask = prompt('タスクを入力してください');
-  tasks.push(newTask);
-  alert('新しいタスクを追加しました。');
-  showAllTasks();
-} 
-
-function showMenu() {
-  prompt('「確認,追加,削除,終了」の４つのいずれかを入力してください');
-}
-
-showAllTasks();
-addTask();
-showMenu();
