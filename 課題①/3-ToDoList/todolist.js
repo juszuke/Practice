4f6ee3d4a8558de20df5600eeb319071e9e70591
juszuke <yusuke.ino.yusuke@gmail.com>
2020-09-14 09:29:00 +0900

    modify practice 1-3

--- 課題①/3-ToDoList/todolist.js
+++ 課題①/3-ToDoList/todolist.js
@@ -6,9 +6,9 @@ function showAllTasks() {
   console.log('=========================');
   console.log('現在持っているのタスク一覧');
   console.log('=========================');
-  for (let i = 0; i < tasks.length; i++) {
-    console.log(`${i} : ${tasks[i]}`);
-  }
+  tasks.forEach ((task, index) => {
+    console.log(`${index} : ${task}`);
+  });
 }
 
 function addTask() {
